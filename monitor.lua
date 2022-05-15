1e9344369c10bf02996be757cc6eca04f3acb46a
JonKAS <kay.kahmann@gmail.com>
2022-05-15 16:41:25 +0200

    Update settings format

--- monitor.lua
+++ monitor.lua
@@ -4,9 +4,22 @@ settings.load()
 local computers = settings.get("computers")
 local monitor = peripheral.wrap("back")
 monitor.setTextScale(0.5)
+
+function getComputer( id )
+    for k, v in pairs(computers) do
+        if(v.id == id) then
+            return v
+        end
+    end
+end
+
+
 while true do
     local computerId, msg = rednet.receive("reporting")
-    local computer = computers[computerId]
+    local computer = getComputer(computerId)
+    local displayMsg = computer.name..": "..msg
+    print(displayMsg)
+    print("Drawing on line "..computer.line)
     monitor.setCursorPos(1, computer.line)
     monitor.clearLine()
     monitor.write(computer.name..": "..msg)
